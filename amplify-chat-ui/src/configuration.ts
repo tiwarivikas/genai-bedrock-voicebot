0bf6e2b7aa7ad28d8baf10f4f0ee705b10a53a4b
Vikas Tiwari <tiwvika@amazon.com>
2024-09-02 21:35:35 +0530

    fix: Env Vars

--- amplify-chat-ui/src/configuration.ts
+++ amplify-chat-ui/src/configuration.ts
@@ -25,12 +25,13 @@ export const defaultConfiguration: Configuration = {
 export const defaultQchatConfiguration: QchatConfiguration = {
   token:
     "",
-  apiUrl: import.meta.env.NEXT_PUBLIC_APIGW_ENDPOINT || '', //Used for Like/Dislike API calls
-  apiStreamingUrl: import.meta.env.NEXT_PUBLIC_STREAMING_API_ENDPOINT || '' , //Used for Streaming Chat Response (default)
+  apiUrl: import.meta.env.VITE_PUBLIC_APIGW_ENDPOINT || '', //Used for Like/Dislike API calls
+  apiStreamingUrl: import.meta.env.VITE_PUBLIC_STREAMING_API_ENDPOINT || '' , //Used for Streaming Chat Response (default)
   apiVersion: "v2",
   streamGetAnswer: true,
   sourcePattern: "{ *doc_idx *: *([^}]*)}",
 }
 
-console.log("NEXT_PUBLIC_APIGW_ENDPOINT:", import.meta.env.NEXT_PUBLIC_APIGW_ENDPOINT);
-console.log("NEXT_PUBLIC_STREAMING_API_ENDPOINT: ", import.meta.env.NEXT_PUBLIC_STREAMING_API_ENDPOINT);
+console.log("NEXT_PUBLIC_APIGW_ENDPOINT:", import.meta.env.VITE_PUBLIC_APIGW_ENDPOINT);
+console.log("NEXT_PUBLIC_STREAMING_API_ENDPOINT: ", import.meta.env.VITE_PUBLIC_STREAMING_API_ENDPOINT);
+console.log("import.meta.env: ", import.meta.env.BASE_URL);
