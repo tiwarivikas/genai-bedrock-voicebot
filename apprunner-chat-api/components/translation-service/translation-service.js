781409b1bfe9755be3115eb935518fa783beede7
Sourabhp151 <125236358+Sourabhp151@users.noreply.github.com>
2024-10-07 13:02:48 +0530

    Update translation-service.js
    adding additional languages

--- apprunner-chat-api/components/translation-service/translation-service.js
+++ apprunner-chat-api/components/translation-service/translation-service.js
@@ -6,7 +6,13 @@ async function translateText(text, sourceLanguageCode, targetLanguageCode) {
 
   if (
     (sourceLanguageCode === "hi" && targetLanguageCode === "en") ||
-    (sourceLanguageCode === "en" && targetLanguageCode === "hi")
+    (sourceLanguageCode === "en" && targetLanguageCode === "hi") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "te") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "ta") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "kn") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "hi") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "pa") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "gu") 
   ) {
     return await awsTranslate(text, sourceLanguageCode, targetLanguageCode);
   } else {
@@ -19,3 +25,28 @@ async function translateText(text, sourceLanguageCode, targetLanguageCode) {
 }
 
 module.exports = { translateText };
+
+
+/*const { awsTranslate } = require("./aws-translate");
+const { bhashiniTranslation } = require("./bhashini-translation");
+
+async function translateText(text, sourceLanguageCode, targetLanguageCode) {
+  if (sourceLanguageCode === targetLanguageCode) return text;
+
+  if (
+    (sourceLanguageCode === "hi" && targetLanguageCode === "en") ||
+    (sourceLanguageCode === "en" && targetLanguageCode === "hi")
+  ) {
+    return await awsTranslate(text, sourceLanguageCode, targetLanguageCode);
+  } else {
+    return await bhashiniTranslation(
+      text,
+      sourceLanguageCode,
+      targetLanguageCode
+    );
+  }
+}
+
+module.exports = { translateText }; */
+
+
